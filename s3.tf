a115af1003dd91c1047c34a55e4b8d03f07d50c0
FaCezar <fa_cezar@hotmail.com>
2024-03-08 18:01:42 -0500

    Adding IAC Scanning Files

--- s3.tf
+++ s3.tf
@@ -10,11 +10,19 @@ resource "random_string" "bucket_suffix" {
 # Create an S3 bucket with a random name
 resource "aws_s3_bucket" "pc-national-bank-bucket" {
   bucket = "pc-national-bank-bucket-${random_string.bucket_suffix.result}"
+  tags = {
+    yor_name  = "pc-national-bank-bucket"
+    yor_trace = "612dfd9e-3a30-4923-9ba6-392d68aff7e8"
+  }
 }
 
 resource "aws_s3_object" "pc-national-bank-bucket-object" {
   for_each = fileset("${path.module}/s3_files", "*")
-  bucket = aws_s3_bucket.pc-national-bank-bucket.bucket
-  key = each.value  
-  source = "${path.module}/s3_files/${each.value}"
+  bucket   = aws_s3_bucket.pc-national-bank-bucket.bucket
+  key      = each.value
+  source   = "${path.module}/s3_files/${each.value}"
+  tags = {
+    yor_name  = "pc-national-bank-bucket-object"
+    yor_trace = "03e48cbb-0aea-4ab4-8778-48bf393cf822"
+  }
 }
--- vpc.tf
+++ vpc.tf
@@ -1,61 +1,71 @@
 resource "aws_vpc" "ebilling-vpc" {
-  cidr_block = "10.10.0.0/16"
+  cidr_block           = "10.10.0.0/16"
   enable_dns_hostnames = true
   tags = {
-      Name = "ebilling ${random_string.bucket_suffix.result} VPC"
-      Stack = "${var.stack-name}"
-      Scenario = "${var.scenario-name}"
+    Name      = "ebilling ${random_string.bucket_suffix.result} VPC"
+    Stack     = "${var.stack-name}"
+    Scenario  = "${var.scenario-name}"
+    yor_name  = "ebilling-vpc"
+    yor_trace = "91a8b4aa-4ad6-4848-96c3-cf977e99c2ed"
   }
 }
 #Internet Gateway
 resource "aws_internet_gateway" "ebilling-internet-gateway" {
   vpc_id = "${aws_vpc.ebilling-vpc.id}"
   tags = {
-      Name = "ebilling ${random_string.bucket_suffix.result} Internet Gateway"
-      Stack = "${var.stack-name}"
-      Scenario = "${var.scenario-name}"
+    Name      = "ebilling ${random_string.bucket_suffix.result} Internet Gateway"
+    Stack     = "${var.stack-name}"
+    Scenario  = "${var.scenario-name}"
+    yor_name  = "ebilling-internet-gateway"
+    yor_trace = "4479ecf3-4d6c-495a-b500-596719ea848c"
   }
 }
 #Public Subnets
 resource "aws_subnet" "ebilling-public-subnet-1" {
   availability_zone = "${var.region}a"
-  cidr_block = "10.10.10.0/24"
-  vpc_id = "${aws_vpc.ebilling-vpc.id}"
+  cidr_block        = "10.10.10.0/24"
+  vpc_id            = "${aws_vpc.ebilling-vpc.id}"
   tags = {
-      Name = "ebilling ${random_string.bucket_suffix.result} Public Subnet #1"
-      Stack = "${var.stack-name}"
-      Scenario = "${var.scenario-name}"
+    Name      = "ebilling ${random_string.bucket_suffix.result} Public Subnet #1"
+    Stack     = "${var.stack-name}"
+    Scenario  = "${var.scenario-name}"
+    yor_name  = "ebilling-public-subnet-1"
+    yor_trace = "7e2561ff-42e9-480b-9d84-c2444addcdf9"
   }
 }
 resource "aws_subnet" "ebilling-public-subnet-2" {
   availability_zone = "${var.region}b"
-  cidr_block = "10.10.20.0/24"
-  vpc_id = "${aws_vpc.ebilling-vpc.id}"
+  cidr_block        = "10.10.20.0/24"
+  vpc_id            = "${aws_vpc.ebilling-vpc.id}"
   tags = {
-      Name = "ebilling ${random_string.bucket_suffix.result} Public Subnet #2"
-      Stack = "${var.stack-name}"
-      Scenario = "${var.scenario-name}"
+    Name      = "ebilling ${random_string.bucket_suffix.result} Public Subnet #2"
+    Stack     = "${var.stack-name}"
+    Scenario  = "${var.scenario-name}"
+    yor_name  = "ebilling-public-subnet-2"
+    yor_trace = "48cb8a22-da09-4686-8d5a-5ebefdfeda57"
   }
 }
 #Public Subnet Routing Table
 resource "aws_route_table" "ebilling-public-subnet-route-table" {
   route {
-      cidr_block = "0.0.0.0/0"
-      gateway_id = "${aws_internet_gateway.ebilling-internet-gateway.id}"
+    cidr_block = "0.0.0.0/0"
+    gateway_id = "${aws_internet_gateway.ebilling-internet-gateway.id}"
   }
   vpc_id = "${aws_vpc.ebilling-vpc.id}"
   tags = {
-      Name = "ebilling ${random_string.bucket_suffix.result} Route Table for Public Subnet"
-      Stack = "${var.stack-name}"
-      Scenario = "${var.scenario-name}"
+    Name      = "ebilling ${random_string.bucket_suffix.result} Route Table for Public Subnet"
+    Stack     = "${var.stack-name}"
+    Scenario  = "${var.scenario-name}"
+    yor_name  = "ebilling-public-subnet-route-table"
+    yor_trace = "f39f8b5e-6050-4fc7-adad-e160a52f9d7e"
   }
 }
 #Public Subnets Routing Associations
 resource "aws_route_table_association" "ebilling-public-subnet-1-route-association" {
-  subnet_id = "${aws_subnet.ebilling-public-subnet-1.id}"
+  subnet_id      = "${aws_subnet.ebilling-public-subnet-1.id}"
   route_table_id = "${aws_route_table.ebilling-public-subnet-route-table.id}"
 }
 resource "aws_route_table_association" "ebilling-public-subnet-2-route-association" {
-  subnet_id = "${aws_subnet.ebilling-public-subnet-2.id}"
+  subnet_id      = "${aws_subnet.ebilling-public-subnet-2.id}"
   route_table_id = "${aws_route_table.ebilling-public-subnet-route-table.id}"
 }
